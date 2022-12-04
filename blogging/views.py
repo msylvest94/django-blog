48cfa1fa90e2d1d80589e782795ad587252b1660
Michael Sylvester <msylvest94@gmail.com>
2022-12-04 13:12:35 -0800

    week 7 changes

--- blogging/views.py
+++ blogging/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
+
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
 
 
 def stub_view(request, *args, **kwargs):
