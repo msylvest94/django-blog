ddc9d6bac92643cf59fb08257599e8cc253f3549
Michael Sylvester <msylvest94@gmail.com>
2022-12-04 12:04:34 -0800

    added stub view

--- blogging/tests.py
+++ blogging/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post
+from blogging.models import Category
+
+
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
 
 
 class PostTestCase(TestCase):
