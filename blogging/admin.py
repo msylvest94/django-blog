f34f7235b629f0a07edd55b88924ab27ef37f285
Michael Sylvester <msylvest94@gmail.com>
2022-12-05 19:31:17 -0800

    completed week 7 assignment

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+
