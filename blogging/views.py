a73ff602c2d624ee66a21b0b4f56e33163e2a75c
Michael Sylvester <msylvest94@gmail.com>
2022-12-12 16:39:05 -0800

    assignment 8 first attempt

--- blogging/views.py
+++ blogging/views.py
@@ -2,22 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+#def list_view(request):
+#    published = Post.objects.exclude(published_date__exact=None)
+#    posts = published.order_by('-published_date')
+#    context = {'posts': posts}
+#    return render(request, 'blogging/list.html', context)
 
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+class PostListView(ListView):
+    queryset = Post.objects.exclude(published_date__isnull=True).order_by('published_date')
+    template_name = 'blogging/list.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+
 
