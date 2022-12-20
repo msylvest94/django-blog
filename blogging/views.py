77889c87634bedf31baf590140dc1d5900ecf350
Michael Sylvester <msylvest94@gmail.com>
2022-12-20 09:10:44 -0800

    added addons 1 and 2, Social Signin/Login with GitHub and Django Model Form for adding posts

--- blogging/views.py
+++ blogging/views.py
@@ -1,25 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+from django.shortcuts import render, redirect
 from blogging.models import Post
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from forms import PostForm
+from datetime import date
 
 
-# def list_view(request):
-#    published = Post.objects.exclude(published_date__exact=None)
-#    posts = published.order_by('-published_date')
-#    context = {'posts': posts}
-#    return render(request, 'blogging/list.html', context)
-
-# def detail_view(request, post_id):
-#    published = Post.objects.exclude(published_date__exact=None)
-#    try:
-#        post = published.get(pk=post_id)
-#    except Post.DoesNotExist:
-#        raise Http404
-#    context = {'post': post}
-#    return render(request, 'blogging/detail.html', context)
+def add_model(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.published_date = date.today()
+            post.save()
+            return redirect('blog_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blogging/add.html', {'form': form})
 
 
 class PostListView(ListView):
